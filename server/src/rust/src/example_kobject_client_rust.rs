7e37efdb4269b71714af866d5de2e3943feba7f1
Timo Nicolai <timonicolai@arcor.de>
2021-07-30 11:09:08 +0200

    Rust: remove L4ReError

--- server/src/rust/src/example_kobject_client_rust.rs
+++ server/src/rust/src/example_kobject_client_rust.rs
@@ -1,19 +1,5 @@
 #![allow(dead_code)]
 
-use std::error::Error;
-use std::fmt;
-
-#[derive(Debug)]
-struct L4ReError(i64);
-
-impl fmt::Display for L4ReError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "L4Re error: {}", self.0)
-    }
-}
-
-impl Error for L4ReError {}
-
 use std::ffi::*;
 use std::os::raw::*;
 
@@ -23,27 +9,21 @@ include!("bind/env_rust.rs");
 include!("bind/example_kobject_shared_rust.rs");
 include!("bind/ipc_types_rust.rs");
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     unsafe {
 
-    let server_name = CString::new("kobject_server")?;
+    let server_name = CString::new("kobject_server").unwrap();
 
     let server = L4ReEnv::env().get_cap_dummy_kobject_2(&server_name);
-    if !server.is_valid() {
-        return Err(Box::new(L4ReError(-1)));
-    }
+    assert!(server.is_valid());
 
     let a = 1;
     let b = 2;
 
     let mut res = 0;
-    if server.access().call(a, b, &mut res) != 0 {
-        return Err(Box::new(L4ReError(-1)));
-    }
+    assert_eq!(server.access().call(a, b, &mut res), 0);
 
     println!("Rust client: result = {}", res);
 
     }
-
-    Ok(())
 }
